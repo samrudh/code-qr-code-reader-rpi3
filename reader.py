1ae4bbd5079a2c970f3a9be6f851edb928ed638a
Samrudha Kelkar <kelkarsamrudha@gmail.com>
2018-12-30 00:23:26 +0530

    Use Picamera to capture image and read  QR code

--- reader.py
+++ reader.py
@@ -1,36 +1,51 @@
+
+import time
+import picamera
+
+
 import zbarlight
 import os
 import sys
 import PIL
+import numpy as np
 
-print 'Taking picture..'
+def get_image():
+  with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('foo.jpg')
+    
+
+
+
+
+
+print ('Taking picture..')
 try:
     f = 1
-    qr_count = len(os.listdir('qr_codes'))
-    os.system('sudo fswebcam -d /dev/video'+sys.argv[1]+' -q qr_codes/qr_'+str(qr_count)+'.jpg')
-    print 'Picture taken..'
-except:
+    #qr_count = len(os.listdir('qr_codes'))
+    get_image()
+    #os.system('sudo fswebcam -d /dev/video'+sys.argv[1]+' -q qr_codes/qr_'+str(qr_count)+'.jpg')
+    print ('Picture taken..')
+except Exception as e:
     f = 0
-    print 'Picture couldn\'t be taken..'
+    print ('Picture couldn\'t be taken with exception ' + str(e))
 
 print
 
 if(f):
-    print 'Scanning image..'
-    f = open('qr_codes/qr_'+str(qr_count)+'.jpg','rb')
+    print ('Scanning image..')
+    f = open('foo.jpg','rb')
     qr = PIL.Image.open(f);
     qr.load()
 
     codes = zbarlight.scan_codes('qrcode',qr)
     if(codes==None):
-        os.remove('qr_codes/qr_'+str(qr_count)+'.jpg')
-        print 'No QR code found'
+        #os.remove('qr_codes/qr_'+str(qr_count)+'.jpg')
+        print ('No QR code found')
     else:
-        print 'QR code(s):'
-        print codes
-        f = open('qr_code_messages.txt','a')
-        for i in range(len(codes)):
-            f.write(codes[i])
-            if(i!=len(codes)-1):
-                f.write('^')
-        f.write('~')
+        print( 'QR code(s):')
+        print (codes)
+        
